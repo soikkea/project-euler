6bc107843cddd59c329c51d07caca2d2777dc8b4
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2019-03-23 13:18:31 +0200

    Solution for problem 01

---
+++ C#/Problems/Problem001.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problems
+{
+    public class Problem001
+    {
+        // https://projecteuler.net/problem=1
+        // Returns the sum of all the multiples of 3 or 5 below n.
+        public static uint run(uint n=1000u)
+        {
+            uint sum = 0;
+
+            for (uint i = n - 1; i > 0; i--)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
